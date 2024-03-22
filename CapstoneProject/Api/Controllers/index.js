5e817f8ef966da1b39e1c24258b656821ab120fd
dylanherbst <dylan.herbstwork@gmail.com>
2024-03-22 13:38:48 +0800

    new api

---
+++ CapstoneProject/Api/Controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  customerController: require("./customerController"),
+  orderController: require("./orderController"),
+  productController: require("./productController"),
+  productController: require("./productController"),
+};
